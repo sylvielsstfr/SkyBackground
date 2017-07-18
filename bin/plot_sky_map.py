76cf228ab7da7c13639a69d4b2c31b680b56b973
dagoret <dagoret@.lal.in2p3.fr>
2017-07-18 20:36:24 +0200

    Must remember how to plot sky background

---
+++ bin/plot_sky_map.py
@@ -0,0 +1,88 @@
+import pylab as plt
+from scipy import *
+from scipy.optimize import leastsq
+import numpy as np
+import matplotlib
+from mpl_toolkits.basemap import Basemap
+import UVspec
+import glob
+from matplotlib.legend import Legend 
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+from matplotlib.font_manager import FontProperties
+
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+
+png=True#False#
+Normalize=True# False #
+sza=30
+fntype = '{:4.1f}'.format(sza)+'_'+'clearsky_reflectivity'
+#fntype = '{:4.1f}'.format(sza)+'_'+'aerosol_default_reflectivity'
+
+if Normalize:
+    pngfile = '../figs/fig_'+fntype.replace('.','_')+'_Normalized.png'
+else:
+    pngfile = '../figs/fig_'+fntype.replace('.','_')+'.png'
+
+fntype = '*'+fntype+'*'
+inpfiles = sort(glob.glob('input/'+fntype))
+outfiles = sort(glob.glob('output/'+fntype+'uu*'))
+
+print inpfiles
+print outfiles
+
+xsize = 20
+ysize = 9
+
+linestyle = ('b', 'b:','g', 'g:','r', 'r:')
+    
+fig = plt.figure(figsize=(xsize,ysize))
+fig.subplots_adjust(left=0.03, right=0.9, bottom=0.04, top=0.92,wspace=0.15)
+#ax = plt.subplot(111)
+m = Basemap(boundinglat=0.,lon_0=0.,resolution=None,projection='npaeqd')
+
+fontsize = 'large'
+
+wavelengths = np.array([350, 450, 620, 750, 880, 980])
+levels = np.array([ 0.001, 0.002, 0.003, 0.004, 0.005, 0.01, 0.02, 0.03, 0.04, 0.05, 0.1, 0.2, 0.3])
+
+plot  = [231,232,233,234,235,236]
+i = 0
+for inpfile,outfile in zip(inpfiles,outfiles):
+
+    phi = UVspec.get_vals(inpfile,'phi')
+    phis = [float(x) for x in phi]
+    umu = UVspec.get_vals(inpfile,'umu')
+    umus = [float(x) for x in umu]
+    umu = np.arccos(np.array((umus)))*180/np.pi-90
+    lat,lon = plt.meshgrid(phis, umu)
+    X,Y = m(lat,lon)
+
+    data = np.genfromtxt(outfile)
+    ax = plt.subplot(plot[i])
+
+    if Normalize:
+        data=data/data.max()
+        levels = np.linspace(0, 1, 11)
+    else:
+        mind = data.min()
+        maxd = data.max()
+        levels = np.linspace(mind, maxd, 100)
+    CS = m.contourf(X,Y,data,levels)
+    m.drawparallels(plt.arange(0,90,20),labels=[1,1,1,1], fontsize=fontsize)
+    m.drawmeridians(plt.arange(0,360,60),labels=[1,1,1,1],labelstyle='+/-', fontsize=fontsize)
+
+    plt.title(r"$\lambda$ = " + str(wavelengths[i])+' nm',
+              fontsize = fontsize, position=(0.5,1.06))
+    if True: #( i == 2 ):
+        CB = plt.colorbar(CS, shrink=0.7)
+        l,b,w,h = plt.gca().get_position().bounds
+        ll,bb,ww,hh = CB.ax.get_position().bounds
+        CB.ax.set_position([ll+0.05*ll, b+0.*h, ww, h*1.])
+
+    i = i + 1
+
+if png:
+    fig.savefig(pngfile, bbox_inches='tight')
+else:
+    plt.figure()
+    plt.show()
---
+++ bin/simulate.py
@@ -0,0 +1,131 @@
+import os
+import math
+import numpy as np
+import UVspec
+
+home = os.environ['HOME']+'/'
+        
+def read_uu_map(fo, nphi, numu):
+
+    # The reading of radiances is a bit complicated. They all come on one line for each
+    # wavelength and altitude when "output_user uu" is specified. In addition, the first
+    # number is for the phi[0], the second for phi[1] and so on. Since we have many umu
+    # and two phis (0 and 180 to get the fill principal plane for umu between 0 and 90)
+    # in the input file the following will unpack, sort and get the right viewing angles.
+    
+
+    f = open(fo,'r')
+    uu = np.zeros((numu,nphi))
+
+    for line in f:
+        l   = line.split()
+        uur = l[6:len(l)]
+    f.close()
+
+    #    print len(uur)
+
+    i  = 0
+    iu = 0
+    while iu < numu:
+        j  = 0
+        while j < nphi:
+            #            print i, iu, j
+            uu[iu][j] = uur[i]
+            i = i + 1
+            j = j + 1
+        iu = iu + 1
+
+    return uu
+
+def write_uu_map(fo, uu, nphi, numu):
+
+    f = open(fo,'w')
+    i  = 0
+    iu = 0
+    while iu < numu:
+        j  = 0
+        while j < nphi:
+            f.write( ' {0:13.6e}'.format(uu[iu][j]))
+            j = j + 1
+        f.write( '\n')
+        iu = iu + 1
+    f.close()
+
+
+#####################################################################
+
+if __name__ == "__main__":
+
+
+    # Set up type of run
+    runtype='clearsky' #'aerosol_default' #
+    sza=60
+    
+    if runtype=='clearsky':
+        outtext='clearsky'
+    elif runtype=='aerosol_default':
+        outtext='aerosol_default'
+        
+    libradtranpath = home+'MacOSX/External/libRadtran'
+    libradtranpathbin = home+'MacOSX/External/libRadtran/'
+    libradtranpathdata = libradtranpath+'/share/libRadtran/data'
+    libradtranpathatm = libradtranpathdata+'/atmmod'
+
+    # Rough estimate of center wavlengths of LSST filters. Should use filter functions
+    # instead.
+    wavelengths = np.array([350, 450, 620, 750, 880, 980])
+
+    for wavelength in wavelengths:
+        verbose=True
+        uvspec = UVspec.UVspec()
+        uvspec.inp["data_files_path"]  =  libradtranpathdata
+        uvspec.inp["atmosphere_file"] = libradtranpathatm+'/afglus.dat'
+        uvspec.inp["albedo"]           = '0.2'
+        uvspec.inp["rte_solver"] = 'disort'
+
+        phis = ''
+        angle = 0.0
+        nphi=0
+        while angle <=360.0:
+            phis = phis+' '+str(angle)
+            angle = angle + 10.0
+            nphi=nphi+1
+
+        umus = ''
+        angle = 180.0
+        numu=0
+        while angle > 90:
+            umu   = math.cos(angle*math.pi/180)
+            umus  = umus+' '+str(umu)
+            angle = angle -2.5
+            numu=numu+1
+        
+
+        if runtype=='aerosol_default':
+            uvspec.inp["aerosol_default"] = ''
+        uvspec.inp["umu"] = umus
+        uvspec.inp["phi"] = phis
+        uvspec.inp["output_user"] = 'lambda zout eglo edir edn eup uu'
+        uvspec.inp["zout"] = 'boa'
+        uvspec.inp["altitude"] = '0.600'
+        uvspec.inp["source"] = 'solar '+libradtranpathdata+'/solar_flux/kurudz_1.0nm.dat'
+        uvspec.inp["sza"]        = str(sza)
+        uvspec.inp["phi0"]       = '0'
+        uvspec.inp["wavelength"]       = str(wavelength)
+        uvspec.inp["output_quantity"] = 'reflectivity' #'transmittance' #
+        uvspec.inp["quiet"] = ''
+
+        if "output_quantity" in uvspec.inp.keys():
+            outtextfinal=outtext+'_'+uvspec.inp["output_quantity"]
+
+        inp = 'input/uvspec_{:5.1f}_{:4.1f}'.format(wavelength,sza)+'_'+outtextfinal+'.inp'
+        out = 'output/uvspec_{:5.1f}_{:4.1f}'.format(wavelength,sza)+'_'+outtextfinal+'.out'
+
+        uvspec.write_input(inp)
+        uvspec.run(inp,out,verbose,path=libradtranpathbin)
+
+        # Read output
+        uu0 = read_uu_map(out,nphi,numu)
+        #
+        out0uu = out+'_uu'
+        write_uu_map(out0uu,uu0,nphi,numu)
